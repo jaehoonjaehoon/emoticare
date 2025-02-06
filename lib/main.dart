bf47188d3a6b98ceb67282b1300673c70decc923
DYO <jjh_2708@naver.com>
2025-02-06 15:01:40 +0900

    스플래시 스크린 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 void main() {
   runApp(EmotiCareApp());
@@ -11,35 +10,7 @@ class EmotiCareApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'EmotiCare',
-      home: SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    // 2초 후 메인 화면으로 이동
-    Timer(Duration(seconds: 2), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => MainScreen()),
-      );
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white, // 배경 색상
-      body: Center(
-        child: Image.asset('assets/logo.png', width: 200), // 로딩 이미지
-      ),
+      home: MainScreen(), // 앱 실행 시 첫 화면을 MainScreen으로 설정
     );
   }
 }
